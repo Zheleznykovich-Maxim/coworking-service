b1968515ebaa0fc149d4fad52e40342df93ad7ef
Maxim Zheleznykovich <maxssmail572@gmail.com>
2024-07-12 17:23:27 +0300

    spring

--- src/main/java/org/example/coworking/config/DatabaseConfig.java
+++ src/main/java/org/example/coworking/config/DatabaseConfig.java
@@ -1,36 +1,44 @@
 package org.example.coworking.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- * Configures and provides a database connection using properties from a configuration file.
- */
+@Configuration
+@PropertySource("classpath:application.yml")
 public class DatabaseConfig {
 
-    /**
-     * Loads database connection properties from a configuration file and returns a DatabaseConnection object.
-     *
-     * @return a DatabaseConnection object configured with the database properties.
-     * @throws IOException if there is an issue with reading the configuration file or connecting to the database.
-     */
-    public static DatabaseConnection getDatabaseConnection() throws IOException {
-        try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("application.properties")) {
-            Properties properties = new Properties();
-            if (input == null) {
-                throw new IOException("Sorry, unable to find application.properties");
-            }
-            properties.load(input);
+    @Value("${db.url}")
+    private String url;
 
-            String url = properties.getProperty("db.url");
-            String username = properties.getProperty("db.username");
-            String password = properties.getProperty("db.password");
+    @Value("${db.username}")
+    private String username;
 
-            return new DatabaseConnection(url, username, password);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw new IOException("Failed to connect to database", e);
-        }
+    @Value("${db.password}")
+    private String password;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setProperties(yamlProperties().getObject());
+        return configurer;
+    }
+
+    @Bean
+    public static YamlPropertiesFactoryBean yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("application.yml"));
+        return bean;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
     }
 }

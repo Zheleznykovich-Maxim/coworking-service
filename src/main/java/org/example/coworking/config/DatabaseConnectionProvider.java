1bfb4628c15de4252ec920cf4a227a2519a28944
Maxim Zheleznykovich <maxssmail572@gmail.com>
2024-07-18 23:56:56 +0300

    delete spring config files

--- src/main/java/org/example/coworking/config/DatabaseConnectionProvider.java
+++
@@ -1,8 +0,0 @@
-package org.example.coworking.config;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DatabaseConnectionProvider {
-    Connection getConnection() throws SQLException;
-}

05d214ad56f15a75c24a952d05ff38aa3ff26dec
Maxim Zheleznykovich <maxssmail572@gmail.com>
2024-07-19 00:30:12 +0300

    updated repository tests

--- src/test/java/booking/BookingRepositoryTest.java
+++ src/test/java/booking/BookingRepositoryTest.java
@@ -1,11 +1,15 @@
 package booking;
 
 import config.TestContainerManager;
-import org.example.coworking.config.DatabaseConnectionProviderImpl;
+import org.example.coworking.MainApplication;
 import org.example.coworking.domain.model.Booking;
 import org.example.coworking.domain.model.enums.ResourceType;
 import org.example.coworking.repository.BookingRepository;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -13,19 +17,24 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(classes = MainApplication.class)
 @Testcontainers
 @DisplayName("Tests for BookingRepository")
 class BookingRepositoryTest {
 
-    private BookingRepository bookingRepository = new BookingRepository(
-            new DatabaseConnectionProviderImpl(
-            postgreSQLContainer.getJdbcUrl(),
-            postgreSQLContainer.getUsername(),
-            postgreSQLContainer.getPassword())
-    );
-
     @Container
     private static final PostgreSQLContainer<?> postgreSQLContainer = TestContainerManager.getPostgresContainer();
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
 
     @BeforeAll
     static void init() {
